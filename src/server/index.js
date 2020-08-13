8bf4462af74f5beeb5e62199e94b64b59cbf3e1f
Lucas Petherbridge <lukexor@gmail.com>
2020-08-12 17:35:53 -0700

    Fixed server code

--- src/server/index.js
+++ src/server/index.js
@@ -1,61 +1,102 @@
-require('dotenv/config')
-const cors = require('cors')
-const express = require('express')
-const asyncHandler = require('express-async-handler')
+require("dotenv/config");
+const cors = require("cors");
+const express = require("express");
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.set('json spaces', 2);
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.set("json spaces", 2);
 
-let appointments = null
+const services = [
+  {
+    id: 1,
+    serviceName: "Replace Brakes",
+    duration: "1 hour",
+  },
+  {
+    id: 2,
+    serviceName: "Oil Change",
+    duration: "30 minutes",
+  },
+  {
+    id: 3,
+    serviceName: "Rotate Tires",
+    duration: "30 minutes",
+  },
+];
 
-app.get('/services', asyncHandler(async (req, res, next) => {
-  const services = await appointments.getAvailableServices();
-  res.send(services)
-}))
+const getRandom = (min, max) => {
+  return Math.floor(Math.random() * max) + min;
+};
 
-app.get('/appointments', asyncHandler(async (req, res, next) => {
-  const appts = await appointments.getAllAppointments()
-  res.send(appts)
-}))
+const uuid = () => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
 
-app.get('/appointments/:serviceType', asyncHandler(async (req, res, next) => {
-  const appts = await appointments.getServiceAppointments(req.params.serviceType)
-  res.send(appts)
-}))
+const randomDate = () => {
+  const start = new Date();
+  const end = new Date(Date.now() + 12096e5);
+  return new Date(
+    start.getTime() + Math.random() * (end.getTime() - start.getTime())
+  );
+};
 
-app.put('/appointments/:id', asyncHandler(async (req, res, next) => {
+let appointments = [];
+services.forEach((service) => {
+  for (let i = 0; i < getRandom(2, 4); ++i) {
+    appointments.push({
+      id: uuid(),
+      serviceName: service.serviceName,
+      serviceId: service.id,
+      start: randomDate().toLocaleString(),
+      duration: service.duration,
+    });
+  }
+});
 
-  const {
-    email, 
-    lastName, 
-    modelYear, 
-    make, 
-    model
-  } = req.body
-  
-  const confirmation = await appointments.bookAppointment(
-    req.params.id,
-    email, 
-    lastName, 
-    modelYear, 
-    make, 
-    model
-  )
-  res.send(confirmation)
-}))
+app.get("/services", (req, res) => res.send(services));
 
-app.get('/raw', asyncHandler(async (req, res, next) => {
-  await appointments.refresh();
-  res.send(appointments.events)
-}))
+app.get("/appointments", (req, res) => res.send(appointments));
 
-// need semicolon
-;(async () => {
-  appointments = await require("./appointments")
-  app.listen(process.env.SERVERPORT, 
-    () => {console.log(`Scheduling server listening on port ${process.env.SERVERPORT}...`)}
-  )
-})()
+app.get("/appointments/:serviceId", (req, res) => {
+  res.send(
+    appointments.filter(
+      (appt) => appt.serviceId === parseInt(req.params.serviceId)
+    )
+  );
+});
 
+app.put("/appointments/:id", (req, res) => {
+  const { email, lastName, modelYear, make, model } = req.body;
+  if (!email) {
+    res.status(400).send(`Invalid email address: ${email}`);
+  }
+  if (!lastName) {
+    res.status(400).send(`Invalid Last Name: ${lastName}`);
+  }
+  if (!modelYear || !make || !model) {
+    res.status(400).send(`Invalid vehicle: ${modelYear} ${make} ${model}`);
+  }
+  const appt = appointments.find((appt) => appt.id === req.params.id);
+  if (!appt) {
+    res.status(400).send("Invalid Appointment ID");
+  }
+  res.send({
+    ...appt,
+    description: `${appt.serviceName} appointment scheduled for:
+Last Name: \t\t${lastName}
+email: \t\t\t${email}
+Date & Time: \t${appt.start}
+Vehicle: \t\t${modelYear} ${make} ${model}`,
+  });
+});
+
+app.listen(process.env.SERVERPORT, () => {
+  console.log(
+    `Scheduling server listening on port ${process.env.SERVERPORT}...`
+  );
+});
